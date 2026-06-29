e2aeca5d2433823efec51a8e1ce93c5525d5e2a1
teemu6388-dot <teemu6388@gmail.com>
2026-06-29 15:39:33 +0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+const CACHE_NAME = "laskurit-v3.1.0";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+
+    "./manifest.json",
+
+    "./css/style.css",
+
+    "./js/app.js",
+    "./js/ui.js",
+    "./js/storage.js",
+    "./js/theme.js",
+
+    "./js/models/project.js",
+    "./js/models/activity.js",
+    "./js/models/vehicle.js",
+    "./js/models/participant.js",
+
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+
+    );
+
+    self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            )
+
+        )
+
+    );
+
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
